b8166d59a76aa9b6e391392742e70298e505ab31
草丛中一杯茶 <l_dw@msn.com>
2021-06-10 22:55:08 +0800

    init

--- configure.sh
+++ configure.sh
@@ -16,7 +16,7 @@ cat << EOF > /usr/local/etc/v2ray/config.json
 {
   "inbounds": [
     {
-    "port":41233,
+    "port":$PORT,
       "tag": "VLESS-in", 
       "protocol": "shadowsocks",  
        "settings": {
